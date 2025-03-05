6c1a3b35fa7688fe397ca48e1664fd01b4af9326
Miquel de Domingo <miquelddg@gmail.com>
2025-03-05 10:19:22 +0100

    fix(core): clear types

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,18 +1,11 @@
 export type FocusedGenerations = 'REGULAR_FOCUSED';
-export type GenerationConfiguration = {
-  excludedDirs: string[];
-  excludedFiles: string[];
-  excludeFreezed: boolean;
-  excludeGenerated: boolean;
-};
 export type GenerationType = FocusedGenerations | RegularGenerations;
-
-export type Maybe<T> = T | undefined;
-
 export type RegularGenerations =
   | 'RECURSIVE'
   | 'REGULAR_SUBFOLDERS'
   | 'REGULAR';
+
+export type Maybe<T> = T | undefined;
 
 export type GenerationConfig = {
   defaultBarrelName: string | undefined;
